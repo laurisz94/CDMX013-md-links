312474897a9a3f14b1f4745a6ef45a5eb62edb81
laurisz94 <lauriisz1994@gmail.com>
2022-11-14 09:57:45 -0600

    finish

--- stats.js
+++ stats.js
@@ -1,19 +1,5 @@
  const v = require('./validate-link.js');
-/* 
-  const getStastsValidate = (links) =>{
-  //let statsLinks = v.validateLink(links);
-  let brokenLinks =  links.filter(e => e.status != 200);
-  //console.log(brokenLinks, 'funciona')
-  let result = {total: links.length, unique: links.length}
-  //console.log(result)
-  return result
- }  */
-  /* const getStast = (links) =>{
 
-    return {total: links.length}
-
-}
- */
 const getStastsValidate = (links) => {
 
   const totalLinks = links.map((link) => link);
@@ -27,18 +13,6 @@ const getStastsValidate = (links) => {
   }
       return stats
   }
- /*  
- // const broken = (links) => {
-    const statsLinks = obtainStats(links);
-    const filterBrokenLink = links.filter(link => link.status !== 200);
-    console.log(links.filter(link => link.status !== 200))
-    let result = {
-        total:links.length,
-        unique: statsLinks.unique,
-        broken: filterBrokenLink.length
-    }
-    console.log(result)
-    return result
-} */
+ 
  
   module.exports = { getStastsValidate}  
